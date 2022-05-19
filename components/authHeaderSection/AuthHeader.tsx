7fbe170b8030346f431347b5a0d87d0d86c3cb5e
Tahgue <taharguemir@gmail.com>
2022-05-19 12:19:38 +0200

    fix auth

--- components/authHeaderSection/AuthHeader.tsx
+++ components/authHeaderSection/AuthHeader.tsx
@@ -34,7 +34,7 @@ export default function AuthHeader() {
       </span>
       {!user && (
         <>
-          <Link href='/signin'>
+          <Link href=''>
             <span className='decoration-1 underline text-primary cursor-pointer p-2 transition-all hover:text-primary hover:scale-110'>
               SignIn
             </span>
